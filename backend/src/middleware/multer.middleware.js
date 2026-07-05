af9c4f94a41d3dad9ed8f704cd6fb4c20c06dc18
Vedant Agarwal <agarwal23vedant@gmail.com>
2026-07-05 13:12:33 +0530

    fix: update multer middleware to use OS temporary directory for file uploads

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,14 +1,29 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// export const upload = multer({
+//   storage,
+// });
+
 import multer from "multer";
+import os from "os";
+import path from "path";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+  destination: (req, file, cb) => {
+    cb(null, os.tmpdir());
   },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}${path.extname(file.originalname)}`);
   },
 });
 
-export const upload = multer({
-  storage,
-});
+export const upload = multer({ storage });
